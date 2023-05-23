2a400e58597a26edb4279a96da1ec5f116e979c8
Phaniharsha Kurukuri <phaniharsha.kurukuri@ADP.com>
2023-05-23 12:02:07 +0530

    Input arguments

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -2,3 +2,12 @@
 
 read -p "Enter your name: " name
 echo "Your Name = $name"
+
+
+##Special variables
+##$0-$n, $*/$@, $#
+
+echo Script name = $0
+echo First Argument = $1
+echo All Argument = $*
+echo Number of Arguments =$#
---
+++ 05-quotes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+##QUOTES
+##single quotes --> Does not consider any character as a special character
+## Very few characters like $ will be considered as special and remaining of them are normal characters.
+echo Apples cost is $100
