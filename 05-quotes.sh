1c71b7e4bd2dedc5594292bc25b39b97eef9f5c2
Phaniharsha Kurukuri <phaniharsha.kurukuri@ADP.com>
2023-05-23 12:05:18 +0530

    single quotes vs double quotes

--- 05-quotes.sh
+++ 05-quotes.sh
@@ -5,4 +5,16 @@
 ##QUOTES
 ##single quotes --> Does not consider any character as a special character
 ## Very few characters like $ will be considered as special and remaining of them are normal characters.
-echo Apples cost is $100
+echo 'Apples cost is $100'
+
+echo "Apples cost is $100"
+
+#if you want to escape in double quotes use \
+
+echo "Apples cost is \$100"
+
+TRANING=DevOps
+echo 'Traning=${TRANING}'
+echo "Traning=${TRANING}"
+
+
