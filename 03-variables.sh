c25aa1ba4d2a445f069d0c724fb129dbb074ac6c
Phaniharsha Kurukuri <phaniharsha.kurukuri@ADP.com>
2023-05-23 10:54:42 +0530

    variables and dynamic variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,15 @@ echo $b Traning
 #{} are needed if variable is combined with other words with out spaces.
 
 
+#all tge time we will not hardcode the value of a variable and we need data dynamically
+
+DATE=2022-03-10
+echo Today date is $DATE
+
+
+DATE=$(date +%F)
+echo Today date is $DATE
+#var=$((expression))
+
+#example
+
