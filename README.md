cd74580717c1bcc82256d74534180f709fe951e2
Phaniharsha Kurukuri <phaniharsha.kurukuri@ADP.com>
2023-05-29 11:47:59 +0530

    making frontend component

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # shellscripting
+
+Problem solving:
+
+1. Installation of nginx failed, but my script still continue.
+2. Installation failed because i am not a root user.
+3. Installation failed because i am not validated that i have root previlages.
+
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,1 +1,25 @@
-echo frontend component
+#!/bin/bash
+
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+
+#Let's download the HTDOCS content and deploy under the Nginx path.
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+
+#Deploy the downloaded content in Nginx Default Location.
+
+rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html/
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+#Finally restart the service once to effect the changes.
+systemctl restart nginx
+systemctl enable nginx
