23537abcda5fb3a382d1028e16d2ed0de7dbd4ca
Phaniharsha Kurukuri <phaniharsha.kurukuri@ADP.com>
2023-06-26 18:25:09 +0530

    updated

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -16,15 +16,15 @@ StatCheck(){
 }
 
 print(){
-  echo -e "|
-           |------------------------$1-------------------" &>>$LOG_FILE
+  echo -e "
+          |------------------------$1-------------------" &>>$LOG_FILE
   echo -e "\e[36m $1 \e[0m"
 }
 
 LOG_FILE=/tmp/roboshop.log
 rm -rf $LOG_FILE
 print "Downloading Nginx"
-yum install nginx -y >>$LOG_FILE
+yum install nginx -y &>>$LOG_FILE
 StatCheck $?
 
 print "Downloading Nginx content"
@@ -35,24 +35,24 @@ StatCheck $?
 #Deploy the downloaded content in Nginx Default Location.
 print "Clean-up old content and extract it"
 
-rm -rf /usr/share/nginx/html/* >>$LOG_FILE
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 StatCheck $?
 
 print "switching nginx directory"
-cd /usr/share/nginx/html/ >>$LOG_FILE
+cd /usr/share/nginx/html/ &>>$LOG_FILE
 
 print "Extracting Archive"
-unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/*  . >>$LOG_FILE
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/*  . &>>$LOG_FILE
 StatCheck $?
 
 print "Update roboshop configuration"
-rm -rf frontend-main README.md >>$LOG_FILE
-mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
+rm -rf frontend-main README.md &>>$LOG_FILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 StatCheck $?
 
 print Starting Nginx
 #Finally restart the service once to effect the changes.
-systemctl restart nginx && systemctl enable nginx >>$LOG_FILE
+systemctl restart nginx && systemctl enable nginx &>>$LOG_FILE
 StatCheck $?
 
 
