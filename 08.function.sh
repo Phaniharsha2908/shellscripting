ab5f9a657692a75a5b243abd95a6d7f15283d2a6
Phaniharsha Kurukuri <phaniharsha.kurukuri@ADP.com>
2023-06-26 18:04:19 +0530

    updated

--- 08.function.sh
+++ 08.function.sh
@@ -16,10 +16,31 @@
 ##Declare a function
 
 print_message(){
-  echo "Hello, welcome to devops training"
+  echo "Hello, welcome to ${1} training"
   echo "I am good to learn"
+  a=20
+  echo "Value of a = $a"
+  b=20
 }
-print_message
 
+STAT(){
+  echo Hello
+  return 1
+  echo Bye
+}
 
+a=10
+print_message Devops
+
+echo "main script argument ${1}"
+STAT
+echo Exist status of function STAT = $?
 ###Note: Function has always been declared first and then you call function later in the code
+
+## Function cannot access the script parameters - you can pass the parameters while calling the function
+
+## normal variables/global variables you can access in the function
+
+## Variable declared in main program can be overwritten in function and vice versa.
+
+## Function is a command, function have exit status as well.
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-if [ "$USER_ID" == 0 ]; then
+if [ "$USER_ID" != 0 ]; then
   echo "you should run your script as sudo or root user"
   exit 1
 fi
 
-echo -e "\e[36m Installing Ngnix \e[0m"
-yum install nginx -y
+StatCheck(){
+  if [ $1 == 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0"
+  else
+    echo -e "\e[31mFAILURE\e[0"
+    exit 2
+  fi
+}
 
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0"
-else
-  echo -e "\e[31mFAILURE\e[0"
-  exit 2
-fi
-echo -e "\e[36m Downloading Nginx content \e[0m"
+print(){
+  echo -e "\e[36 $1 \e[0m"
+}
+
+LOG_FILE=/tmp/roboshop.log
+rm -rf $LOG_FILE
+print "Downloading Nginx"
+yum install nginx -y
+StatCheck $?
+
+print "Downloading Nginx content"
 #Let's download the HTDOCS content and deploy under the Nginx path.
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0"
-else
-  echo -e "\e[31mFAILURE\e[0"
-  exit 2
-fi
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
+StatCheck $?
 
 #Deploy the downloaded content in Nginx Default Location.
-echo -e "\e[36m Clean-up old content and extract it \e[0m"
+print "Clean-up old content and extract it"
 
-rm -rf /usr/share/nginx/html/*
-cd /usr/share/nginx/html/
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0"
-else
-  echo -e "\e[31mFAILURE\e[0"
-  exit 2
-fi
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
+StatCheck $?
+
+Print "switching nginx directory"
+cd /usr/share/nginx/html/ >>$LOG_FILE
+
+print "Extracting Archive"
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . >>$LOG_FILE
+StatCheck $?
+
+print "Update roboshop configuration"
+rm -rf frontend-main README.md >>$LOG_FILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
+StatCheck $?
+
 echo -e "\e[36 Starting Nginx \e[0"
 #Finally restart the service once to effect the changes.
-systemctl restart nginx
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0"
-else
-  echo -e "\e[31mFAILURE\e[0"
-  exit 2
-fi
-systemctl enable nginx
+systemctl restart nginx && systemctl enable nginx >>$LOG_FILE
+StatCheck $?
+
+
